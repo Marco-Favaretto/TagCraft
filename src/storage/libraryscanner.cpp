85b8c87ed628a2f6368d2678bb6de8302e14f4c8
Marco-Favaretto <favaretto801@gmail.com>
2026-08-21 17:36:07 +0200

    LibraryScanner::smartScan

--- src/storage/libraryscanner.cpp
+++ src/storage/libraryscanner.cpp
@@ -24,21 +24,52 @@ QList<TrackFileSystemDto> LibraryScanner::scanAudioFiles(const QString& musicFol
 }
 
 ScanResultDto LibraryScanner::smartScan(const QString& musicFolderPath) {
-    QHash<QString, TrackFileSystemDto> states = TrackDao::getAllFileStates();
+    QHash<QString, TrackFileSystemDto> states = TrackDao::getAllFileStates(); // HashMap<relativePath, TrackFileSystemDto>
     ScanResultDto dto;
-    QDirIterator it(musicFolderPath, QStringList() << "*.mp3", QDir::Files, QDirIterator::Subdirectories);
+    QSet<QString> foundPaths;
+    QDirIterator it(
+        musicFolderPath,
+        QStringList() << "*.mp3",
+        QDir::Files,
+        QDirIterator::Subdirectories
+    );
 
-    while(it.hasNext()) {
+    while (it.hasNext()) {
         it.next();
         QFileInfo info = it.fileInfo();
-
         QString relPath = StorageManager::instance().toRelativePath(info.absoluteFilePath());
+        foundPaths.insert(relPath);
 
-        // if relPath not in Map -> newSong
-        // if relpath in map but lastmodified diff -> changed
-        // if relpath in map but not in iterator -> deleted
-        // if relpath in map and lastmodified equals -> unchanged
+        if (!states.contains(relPath)) { // NEW: if relPath not in Map -> newSong
+            dto.newTracks.append({
+                relPath,
+                info.size(),
+                info.lastModified().toSecsSinceEpoch()
+            });
+            qDebug() << "Track" << relPath << "nuova";
+        } else {
+            const TrackFileSystemDto& oldTrack = states[relPath];
+            if (info.lastModified().toSecsSinceEpoch() != oldTrack.lastModified || info.size() != oldTrack.fileSize) { // MODIFIED: if relpath in map but lastmodified diff -> changed
+                dto.modifiedTracks.append({
+                    relPath,
+                    info.size(),
+                    info.lastModified().toSecsSinceEpoch()
+                });
+                qDebug() << "Track" << relPath << "modificata";
+            } else { // UNCHANGED:  if relpath in map and lastmodified equals -> unchanged
+                dto.unchangedTracks.append(relPath);
+                qDebug() << "Track" << relPath << "non modificata";
+            }
+        }
+    }
 
+    // DELETED: if relpath in map but not in DB/iterator -> deleted
+    for (auto it = states.constBegin(); it != states.constEnd(); ++it) {
+        if (!foundPaths.contains(it.key())) {
+            dto.deletedTracks.append(it.key());
+
+            qDebug() << "Track" << it.key() << "eliminata";
+        }
     }
 
     return dto;
